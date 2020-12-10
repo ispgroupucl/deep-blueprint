6235dff1ba18741f25a8333fc14ff5011e04f0df
Victor Joos <victor.joos@uclouvain.be>
2020-12-10 11:45:58 +0100

    Adds host resolvers

--- bioblue/__init__.py
+++ bioblue/__init__.py
@@ -1,4 +1,15 @@
 __version__ = "0.1.0"
 
-from bioblue.nb import *
 from bioblue import dataset, transforms, model
+
+from omegaconf import OmegaConf
+import platform
+
+OmegaConf.register_resolver("host", lambda: platform.node().split(".", 1)[0])
+OmegaConf.register_resolver("uname", lambda x: getattr(platform.uname(), x))
+
+__all__ = [
+    "dataset",
+    "transforms",
+    "model",
+]
