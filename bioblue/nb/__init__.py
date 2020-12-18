dda059b8a21184e002ca8b7f54ce1396686b864c
Victor Joos <victor.joos@uclouvain.be>
2020-12-18 09:59:15 +0100

    added image visu

--- bioblue/nb/__init__.py
+++ bioblue/nb/__init__.py
@@ -25,8 +25,8 @@ import pytorch_lightning as pl
 
 import bioblue as bb
 
-from bioblue.nb.image_display import MultiImageDisplay
-
+from bioblue.nb.image_display import MultiImageDisplay, myshow, myshow3d
+from bioblue.nb.load import load_from_cfg, load_from_runid
 
 # Initialize environment variables
 with init_hydra(config_module="bioblue.conf"):
---
+++ bioblue/nb/load.py
@@ -0,0 +1,45 @@
+from typing import Any, Dict, Optional, Tuple
+from hydra.utils import instantiate
+from mlflow.tracking.client import MlflowClient
+from omegaconf import OmegaConf, DictConfig
+import mlflow
+import pytorch_lightning as pl
+from tempfile import TemporaryDirectory
+from importlib import import_module
+
+
+def load_from_runid(run_id: str, ckpt_name: str = "epoch"):
+    mlflow_client = MlflowClient()
+    run = mlflow.get_run(run_id)
+
+    with TemporaryDirectory() as directory:
+        configfile = mlflow_client.download_artifacts(
+            run_id, "config/config.yaml", directory
+        )
+        modelfile = None
+        for modelinfo in mlflow_client.list_artifacts(run_id, path="models"):
+            if ckpt_name in modelinfo.path:
+                modelfile = mlflow_client.download_artifacts(
+                    run_id, modelinfo.path, dst_path=directory
+                )
+                break
+        if modelfile is None:
+            raise ValueError(
+                f"Either ckpt_name ({ckpt_name}) is invalid or another error occured."
+            )
+        cfg = OmegaConf.load(configfile)
+        target_module, target_class = cfg.module["_target_"].rsplit(".", 1)
+        target_module = import_module(target_module)
+        target_class = getattr(target_module, target_class)
+        module = target_class.load_from_checkpoint(modelfile)
+        datamodule = instantiate(cfg.dataset)
+        datamodule.prepare_data()
+        datamodule.setup()
+    return cfg, module, datamodule
+
+
+def load_from_cfg(cfg: DictConfig) -> Tuple[pl.LightningModule, pl.LightningDataModule]:
+    datamodule = instantiate(cfg.dataset)
+    module = instantiate(cfg.module)
+
+    return module, datamodule
