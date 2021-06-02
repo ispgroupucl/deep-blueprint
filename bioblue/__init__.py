f69c32e0f41bafcb46592a3208618e7bc022e3d5
Victor Joos <victorjoosdtb@gmail.com>
2021-06-02 11:48:03 +0200

    host to lower

--- bioblue/__init__.py
+++ bioblue/__init__.py
@@ -6,7 +6,7 @@ from bioblue.utils import git_info
 from omegaconf import OmegaConf
 import platform
 
-OmegaConf.register_resolver("host", lambda: platform.node().split(".", 1)[0])
+OmegaConf.register_resolver("host", lambda: platform.node().split(".", 1)[0].lower())
 OmegaConf.register_resolver("uname", lambda x: getattr(platform.uname(), x))
 OmegaConf.register_resolver("git", git_info)
 
