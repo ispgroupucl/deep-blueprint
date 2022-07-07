d81131eefd3f12d8a7d0b8bbe974a6109ff453fe
NielsSayez <niels.sayez@uclouvain.be>
2022-07-07 20:56:02 +0200

    ResnetClassifierV3
    Former-commit-id: 35a2f02a541f905b136e11fade1276684b8a6677

--- bioblue/model/ResnetClassifier.py
+++ bioblue/model/ResnetClassifier.py
@@ -9,6 +9,88 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 
+class ResNetClassifierV3(nn.Module):
+    def __init__(self, 
+                model_cfg,
+                input_format,
+                output_format,
+                classes, 
+                architecture=None,
+                tune_fc_only=True):
+        super().__init__()
+
+        self.__dict__.update(locals())
+        resnets = {
+            '18': models.resnet18, '34': models.resnet34,
+            '50': models.resnet50, '101': models.resnet101,
+            '152': models.resnet152
+        }
+        
+
+        self.classes = classes
+        self.n_classes = len(classes)
+
+        self.architecture = architecture
+        
+        self.resnet_version=self.architecture['resnet_version']
+        self.pretrained = self.architecture['pretrained']
+        self.tune_fc_only = self.architecture['tune_fc_only']
+
+        self.input_format = input_format
+
+        self.fcn_input_format = [ inp for inp in self.input_format if inp != 'image']
+        self.input_format = [inp for inp in self.input_format if inp == 'image']
+
+        # Using a pretrained ResNet backbone
+        self.resnet_model = resnets[self.resnet_version](pretrained=self.pretrained)
+        # Replace old FC layer with Identity so we can train our own
+        linear_size = list(self.resnet_model.children())[-1].in_features
+        # replace final layer for fine tuning
+        self.resnet_model.fc = nn.Identity()
+
+        print('linear_size', linear_size)
+
+        initial_fcn_layer = nn.Linear(linear_size + len(self.fcn_input_format),self.architecture['fcn_width'])
+        hidden_fcn_layers = [ nn.Sequential(
+                                    nn.Linear(self.architecture['fcn_width'],self.architecture['fcn_width']),
+                                    nn.ReLU()
+                                )
+                            for i in range(architecture["fcn_num_hidden"])]
+        fcn2class_layer = nn.Linear(self.architecture['fcn_width'],self.n_classes)
+
+        self.classifier_fcn = nn.Sequential(
+            initial_fcn_layer,
+            nn.ReLU(),
+            *hidden_fcn_layers,
+            fcn2class_layer
+        )
+
+        if tune_fc_only: # option to only tune the fully-connected layers
+            for child in list(self.resnet_model.children())[:-1]:
+                for param in child.parameters():
+                    param.requires_grad = False
+
+    def forward(self, X):
+        # print('ResnetClassifierV2', [(key , X[key].shape, X[key].device ) for  key in X.keys()])
+
+        resnet_input = X['image'].repeat(1,3,1,1)
+        # print('resnet_input', resnet_input.shape)
+        resnet_output = self.resnet_model(resnet_input)
+
+        print('linear_size', resnet_output.shape)
+
+        fcn_inputs = []
+        for dtype in self.fcn_input_format:
+            fcn_inputs.append(torch.squeeze(X[dtype],2))
+        fcn_inputs = torch.cat(fcn_inputs, 1 )
+        print("fcn_inputs: ",fcn_inputs.shape)
+        fcn_inputs = torch.cat([fcn_inputs, resnet_output], 1 )
+        print("FULL_fcn_inputs: ",fcn_inputs.shape)
+
+        fc_nx = self.classifier_fcn(fcn_inputs)
+
+
+        return fc_nx
 
 class ResNetClassifierV2(nn.Module):
     def __init__(self, 
--- bioblue/model/__init__.py
+++ bioblue/model/__init__.py
@@ -1,6 +1,6 @@
 from bioblue.model.unet import Unet
 from .conf_unet import ConfUnet
 from .conf_resnet import ConfResNet
-from .ResnetClassifier  import ResNetClassifier, ResNetClassifierV2
+from .ResnetClassifier  import ResNetClassifier, ResNetClassifierV2, ResNetClassifierV3
 from .model_wrapper import ModelWrapper
 from .utils import ModelConfig, Model3dConfig
